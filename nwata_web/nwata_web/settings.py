25479ed6e3ea181d9025dad1f198bac5f4fecd32
Curtis-Lee Waterboer <waterboercurtislee@gmail.com>
2026-01-08 07:32:55 +0000

    Production DB setup

--- nwata_web/nwata_web/settings.py
+++ nwata_web/nwata_web/settings.py
@@ -13,9 +13,13 @@ For the full list of settings and their values, see
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
